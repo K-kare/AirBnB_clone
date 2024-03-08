c64576f07dca929033640d6db00f165c044788fc
K-kare <kamunawangari@gmail.com>
2024-03-08 03:43:38 -0800

    file_storage.py

--- console.py
+++ console.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 #command interpreter in python
 import cmd
+from models import storage
+import json
 
-class Myconsole(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
     def do_EOF(self, args):
         """shows end of a file"""
@@ -11,5 +13,38 @@ class Myconsole(cmd.Cmd):
     def do_quit(self, args):
         """This exits the program"""
         return True
+    def emptyline(self):
+        """Doesn't do anything on ENTER.
+        """
+        pass
+    def do_create(self, line):
+        """Creates an instance.
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            b = storage.classes()[line]()
+            b.save()
+            print(b.i)
+    def do_show(self, line):
+        """Prints the string representation of an instance.
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(words) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(words[0], words[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[key])
+
 if __name__ == '__main__':
-    Myconsole().cmdloop()
+    HBNBCommand().cmdloop()
